492ae894612d1a78396feb76b7356b7aa9bd6075
jgds <jerome.gds@rosaly.com>
2023-05-10 00:52:52 +0200

    init react page

--- react-page/src/functions/helpers.js
+++ react-page/src/functions/helpers.js
@@ -1,8 +1,8 @@
 //const kvDataKey = 'monitors_data_v1_1'
 
 export function getKVMonitors() {
-  return {"lastUpdate":{"allOperational":true,"time":1683591917167,"loc":"WAW"},"monitors":{"rosaly-app":{"firstCheck":"2023-05-09","lastCheck":{"status":200,"statusText":"OK","operational":true},"checks":{"2023-05-09":{"fails":0,"res":{"WAW":{"n":14,"ms":2430,"a":174}}}}},"companies-admin":{"firstCheck":"2023-05-09","lastCheck":{"status":200,"statusText":"OK","operational":true},"checks":{"2023-05-09":{"fails":0,"res":{"WAW":{"n":14,"ms":2495,"a":178}}}}}}}
-  //return KV_STATUS_PAGE.get(kvDataKey, 'json')
+  //return {"lastUpdate":{"allOperational":true,"time":1683591917167,"loc":"WAW"},"monitors":{"rosaly-app":{"firstCheck":"2023-05-09","lastCheck":{"status":200,"statusText":"OK","operational":true},"checks":{"2023-05-09":{"fails":0,"res":{"WAW":{"n":14,"ms":2430,"a":174}}}}},"companies-admin":{"firstCheck":"2023-05-09","lastCheck":{"status":200,"statusText":"OK","operational":true},"checks":{"2023-05-09":{"fails":0,"res":{"WAW":{"n":14,"ms":2495,"a":178}}}}}}}
+  return TEST.get(kvDataKey, 'json')
 }
 
 export async function getCheckLocation() {
