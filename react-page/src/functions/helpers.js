4173ef062b53754cb3816edbcb2266cfc0748190
jgds <jerome.gds@rosaly.com>
2023-05-10 01:03:00 +0200

    init react page

--- react-page/src/functions/helpers.js
+++ react-page/src/functions/helpers.js
@@ -2,7 +2,7 @@
 
 export function getKVMonitors() {
   //return {"lastUpdate":{"allOperational":true,"time":1683591917167,"loc":"WAW"},"monitors":{"rosaly-app":{"firstCheck":"2023-05-09","lastCheck":{"status":200,"statusText":"OK","operational":true},"checks":{"2023-05-09":{"fails":0,"res":{"WAW":{"n":14,"ms":2430,"a":174}}}}},"companies-admin":{"firstCheck":"2023-05-09","lastCheck":{"status":200,"statusText":"OK","operational":true},"checks":{"2023-05-09":{"fails":0,"res":{"WAW":{"n":14,"ms":2495,"a":178}}}}}}}
-  return TEST.get(kvDataKey, 'json')
+  return KV_STATUS_PAGE.get(kvDataKey, 'json')
 }
 
 export async function getCheckLocation() {
