729b2d4317b7c8c8b47e7fe66074f275491c826c
jgds <jerome.gds@rosaly.com>
2023-05-10 12:44:07 +0200

    init react page

--- react-page/functions/data.js
+++ react-page/functions/data.js
@@ -1,14 +1,10 @@
-export function getKVMonitors(kvNamespace) {
+function getKVMonitors(kvNamespace) {
   //return {"lastUpdate":{"allOperational":true,"time":1683591917167,"loc":"WAW"},"monitors":{"rosaly-app":{"firstCheck":"2023-05-09","lastCheck":{"status":200,"statusText":"OK","operational":true},"checks":{"2023-05-09":{"fails":0,"res":{"WAW":{"n":14,"ms":2430,"a":174}}}}},"companies-admin":{"firstCheck":"2023-05-09","lastCheck":{"status":200,"statusText":"OK","operational":true},"checks":{"2023-05-09":{"fails":0,"res":{"WAW":{"n":14,"ms":2495,"a":178}}}}}}}
   return kvNamespace.get('monitors_data_v1_1')
 }
 
 export async function onRequest(context) {
-  const myHeaders = new Headers();
-
-  myHeaders.append("Content-Type", "application/json");
-
-  const value = await env.NAMESPACE.list();
+  const value = await context.env.KV_STATUS_PAGE.list();
 
   return new Response(value.keys)
   return new Response(getKVMonitors(context.env.KV_STATUS_PAGE), myHeaders)
